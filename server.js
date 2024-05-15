2431133b0e8430d7b0886918a158ba9b2a328b70
Jerom <jeromjomanthara@gmail.com>
2024-05-15 20:05:13 +0530

    feat(routes): add routes to server

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const dotenv = require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/v1/contacts", require("./routes/contactRoutes"));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
