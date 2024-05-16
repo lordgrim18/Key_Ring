1b55b8b4ee675b100c92218cba9f872d04f04c12
Jerom <jeromjomanthara@gmail.com>
2024-05-16 13:49:54 +0530

    feat(models): add contact model

---
+++ models/contactModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: [true, 'Name is required']
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required']
+        },
+        phone: {
+            type: String,
+            required: [true, 'Phone is required']
+        },
+        created_at: {
+            type: Date,
+            default: Date.now
+        }
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Contact', contactSchema);
