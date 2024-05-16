3c7638cae426cb8c3f7d848ced265f08d211dbf5
Jerom <jeromjomanthara@gmail.com>
2024-05-16 16:22:55 +0530

    feat(route): add single contact get route and controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -28,4 +28,22 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(201).json({ success: true, msg: 'Created new contact', data: contact});
 });
 
-module.exports = { getContact, createContact };
+
+// @desc Get a contact
+// @route GET /api/v1/contacts/:id
+// @access Public
+const getContactById = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error('Contact not found');
+    }
+    res.status(200).json({ success: true, msg: 'Show contact', data: contact });
+});
+
+
+module.exports = { 
+    getContact, 
+    createContact,
+    getContactById,
+};
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getContact, createContact } = require('../controllers/contactController');
+const { 
+    getContact, 
+    createContact,
+    getContactById,
+} = require('../controllers/contactController');
 
 router.route('/').get(getContact).post(createContact);
+router.route("/:id").get(getContactById)
 
 
 module.exports = router;
