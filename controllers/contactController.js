9a281c258eb3b0c6cfd71dc734a7572b2d0f53fe
Jerom <jeromjomanthara@gmail.com>
2024-05-16 16:15:51 +0530

    perf(controller): add models to each function

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,24 +1,31 @@
 const asyncHandler = require('express-async-handler');
+const Contact = require('../models/contactModel');
 
 // @desc Get all contacts
 // @route GET /api/v1/contacts
 // @access Public
 
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all contacts' });
+    const contacts = await Contact.find();
+    res.status(200).json({ success: true, msg: 'Show all contacts', data: contacts });
 });
 
 // @desc Create a contact
 // @route POST /api/v1/contacts
 // @access Public
 const createContact = asyncHandler(async (req, res) => {
-    console.log(req.body);
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
         res.status(400);
         throw new Error('Please fill all fields!!');
     }
-    res.status(201).json({ success: true, msg: 'Create new contact' });
+
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    });
+    res.status(201).json({ success: true, msg: 'Created new contact', data: contact});
 });
 
 module.exports = { getContact, createContact };
