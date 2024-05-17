6029303a02ea3533a921cf04930ae07136d9a0e1
Jerom <jeromjomanthara@gmail.com>
2024-05-17 11:11:52 +0530

    feat(auth): add login route and controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const { model } = require('mongoose');
 
@@ -41,4 +42,53 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { registerUser };
+// @desc Login a user
+// @route POST /api/v1/auth/login
+// @access Public
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400);
+      throw new Error("No User with this email found!");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400);
+      throw new Error("Invalid Password for given user!");
+    }
+  
+    const token = jwt.sign(
+      {
+      user: {
+          id: user._id,
+          email: user.email, 
+        },
+      }, 
+      process.env.ACCESS_TOKEN_SECRET, 
+      {
+      expiresIn: "30d",
+      }
+    );
+    if (token) {
+      res.status(200).json({
+        success: true,
+        msg: "User logged in successfully",
+        data: { _id: user.id, email: user.email, token: token },
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid credentials!");
+    }
+});
+
+
+module.exports = { 
+  registerUser,
+  loginUser, 
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {
     registerUser,
+    loginUser,
 } = require('../controllers/authController');
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
