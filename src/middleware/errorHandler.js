6114550460716979acebed783898d07e81d98720
Jerom <jeromjomanthara@gmail.com>
2024-05-19 13:40:47 +0530

    perf(middleware): remove stack trace from errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -7,35 +7,30 @@ const errorHandler = (err, req, res, next) => {
             res.json({ 
                 title: 'Validation Error',
                 message: err.message,
-                stackTrace: err.stack,
             });
             break;
         case constants.NOT_FOUND:
             res.json({ 
                 title: 'Not Found',
                 message: err.message,
-                stackTrace: err.stack,
             });
             break;
         case constants.SERVER_ERROR:
             res.json({ 
                 title: 'Server Error',
                 message: err.message,
-                stackTrace: err.stack,
             });
             break;
         case constants.UNAUTHORIZED:
             res.json({ 
                 title: 'Unauthorized',
                 message: err.message,
-                stackTrace: err.stack,
             });
             break;
         case constants.FORBIDDEN:
             res.json({ 
                 title: 'Forbidden',
                 message: err.message,
-                stackTrace: err.stack,
             });
             break;
         default:
