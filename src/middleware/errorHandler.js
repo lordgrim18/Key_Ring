4b614fb02560dd91b5be81cabd9f6d098b550312
Jerom <jeromjomanthara@gmail.com>
2024-05-20 00:39:34 +0530

    refactor(utils): move contants to utils

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,4 @@
-const {constants} = require('../constants');
+const {constants} = require('../utils/constants');
 
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : constants.SERVER_ERROR;
