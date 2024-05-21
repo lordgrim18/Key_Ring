512cec312148988a568d7bcb49f43771c69c6e5c
Jerom <jeromjomanthara@gmail.com>
2024-05-21 23:04:25 +0530

    perf(middleware): make response of errorHandler constant

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -5,37 +5,39 @@ const errorHandler = (err, req, res, next) => {
     switch (statusCode) {
         case constants.VALIDATION_ERROR:
             res.json({ 
-                title: err.message ? 'Error' : 'Validation Error',
-                message: 'Validation error',
-                errors: err.errors || err.message,
+                title: 'Validation Error',
+                error: err.errors || err.message,
               });
             break;
         case constants.NOT_FOUND:
             res.json({ 
                 title: 'Not Found',
-                message: err.message,
+                error: err.message,
             });
             break;
         case constants.SERVER_ERROR:
             res.json({ 
                 title: 'Server Error',
-                message: err.message,
+                error: err.message,
             });
             break;
         case constants.UNAUTHORIZED:
             res.json({ 
                 title: 'Unauthorized',
-                message: err.message,
+                error: err.message,
             });
             break;
         case constants.FORBIDDEN:
             res.json({ 
                 title: 'Forbidden',
-                message: err.message,
+                error: err.message,
             });
             break;
         default:
-            break;
+            res.json({ 
+                title: 'Server Error',
+                error: err.message,
+            });
     }
 
 };
