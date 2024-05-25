d72cf8cf884f0f58f12e8f454416edaf5e00905a
Jerom <jeromjomanthara@gmail.com>
2024-05-25 18:26:05 +0530

    test(e2e): test invalid user login

--- src/__test__/e2e/user.test.js
+++ src/__test__/e2e/user.test.js
@@ -105,6 +105,52 @@ describe("create user and login", () => {
             expect(response.body.data).toHaveProperty("token");
     });
 
+    it("should fail if user does not exist", async () => {
+        const response = await request(app)
+            .post("/api/v1/auth/login")
+            .send({
+                email: "john@email.com",
+                password: "password123",
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty("success", false);
+            expect(response.body).toHaveProperty("title", "Validation Error");
+            expect(response.body).toHaveProperty("error", "No User with this email found!");
+    });
+
+    it("should fail if password is incorrect", async () => {
+        const response = await request(app)
+            .post("/api/v1/auth/login")
+            .send({
+                email: data.email,
+                password: "password",
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty("success", false);
+            expect(response.body).toHaveProperty("title", "Validation Error");
+            expect(response.body).toHaveProperty("error", "Invalid Password for given user!");
+
+    });
+
+    it("should fail if required fields are missing", async () => {
+        const response = await request(app)
+            .post("/api/v1/auth/login")
+            .send({});
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty("success", false);
+            expect(response.body).toHaveProperty("title", "Validation Error");
+            expect(response.body.error).toHaveLength(2);
+
+            const error = response.body.error[0];
+            expect(error).toHaveProperty("field", "email");
+            expect(error).toHaveProperty("message", "Email is required, Enter proper email address");
+
+            const error1 = response.body.error[1];
+            expect(error1).toHaveProperty("field", "password");
+            expect(error1).toHaveProperty("message", "Password is required");
+    });
 
     afterAll(async () => {
         await mongoose.connection.dropDatabase();
