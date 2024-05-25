4c3fef14b5f9a397eb7a77ac7e177906fbf70529
Jerom <jeromjomanthara@gmail.com>
2024-05-25 14:05:30 +0530

    test(e2e.user): test invalid user registration

--- src/__test__/e2e/user.test.js
+++ src/__test__/e2e/user.test.js
@@ -20,6 +20,54 @@ describe("create user and login", () => {
         password: "password123",
     };
 
+    it("should fail if required fields are missing", async () => {
+        const response = await request(app)
+            .post("/api/v1/auth/register")
+            .send({});
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty("success", false);
+            expect(response.body).toHaveProperty("title", "Validation Error");
+            expect(response.body.error).toHaveLength(3);
+
+            const error = response.body.error[0];
+            expect(error).toHaveProperty("field", "email");
+            expect(error).toHaveProperty("message", "Email is required, Enter proper email address");
+
+            const error1 = response.body.error[1];
+            expect(error1).toHaveProperty("field", "name");
+            expect(error1).toHaveProperty("message", "Name is required, Name should be at least 3 chars long");
+
+            const error2 = response.body.error[2];
+            expect(error2).toHaveProperty("field", "password");
+            expect(error2).toHaveProperty("message", "Password is required, Password should be at least 6 chars long");
+    });
+
+    it("should fail if entered fields are invalid", async () => {
+        const response = await request(app)
+            .post("/api/v1/auth/register")
+            .send({
+                name: "Jo",
+                email: "johndoe",
+                password: "pass",
+            });
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty("success", false);
+            expect(response.body).toHaveProperty("title", "Validation Error");
+            expect(response.body.error).toHaveLength(3);
+
+            const error = response.body.error[0];
+            expect(error).toHaveProperty("field", "email");
+            expect(error).toHaveProperty("message", "Enter proper email address");
+
+            const error1 = response.body.error[1];
+            expect(error1).toHaveProperty("field", "name");
+            expect(error1).toHaveProperty("message", "Name should be at least 3 chars long");
+
+            const error2 = response.body.error[2];
+            expect(error2).toHaveProperty("field", "password");
+            expect(error2).toHaveProperty("message", "Password should be at least 6 chars long");
+    });
+
     it("should create a new user", async () => {
         const response = await request(app)
             .post("/api/v1/auth/register")
@@ -29,6 +77,20 @@ describe("create user and login", () => {
             expect(response.body).toHaveProperty("msg", "User created successfully");
             expect(response.body.data).toHaveProperty("_id");
             expect(response.body.data).toHaveProperty("email", data.email);
+    });
+
+    it("should fail if user already exists", async () => {
+        const response = await request(app)
+            .post("/api/v1/auth/register")
+            .send( data );
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty("success", false);
+            expect(response.body).toHaveProperty("title", "Validation Error");
+            
+            const error = response.body.error[0];
+            expect(error).toHaveProperty("field", "email");
+            expect(error).toHaveProperty("value", data.email);
+            expect(error).toHaveProperty("message", "Email already in use");
     });
 
     it("should login a user", async () => {
