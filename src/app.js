722ad9190c3b7b70a07566bd69adcb79c9f0f3a9
Jerom <jeromjomanthara@gmail.com>
2024-07-07 12:41:16 +0530

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/contacts", require("./routes/contactRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
